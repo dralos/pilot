8cdbea86fb0551fcbda1ef7859c0d6028c150537
dralos <luisma.cobas@gmail.com>
2023-04-08 16:25:05 +0200

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [vue()],
   server: {
     port: process.env.API_PORT || 8081
   },
   build: {
-    outDir: '/var/www/pilot-web'
+    // outDir: '/var/www/pilot-web'
   },
   define: {
     __VUE_I18N_FULL_INSTALL__: true,
